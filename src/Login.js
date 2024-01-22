f2229f5c197f05bbffa6ea19c699e18c30296ca8
Johnkennedy <113602917+johnkennedyb@users.noreply.github.com>
2024-01-22 14:55:10 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import './login.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; // Include Bootstrap CSS
+import './login.css'; // Your custom styles
 
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState('');
@@ -24,32 +25,47 @@ const Login = ({ onLogin }) => {
   }, []);
 
   return (
-    <div className="login-container">
-      <h2>Login To Your Account</h2>
-      <form>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="button" onClick={handleLogin} role="button">
-          Login
-        </button>
-      </form>
-      
+    <div className="container h-100">
+      <div className="row align-items-center h-100">
+        <div className="col-md-6 mx-auto">
+          <div className="login-container">
+            <h2>Login To Your Account</h2>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">
+                  Username:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
